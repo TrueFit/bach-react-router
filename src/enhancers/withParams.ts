b441e0612060f24056c51af6ff7a69ed7d0a8c8f
Josh Gretz <jgretz@truefit.io>
2021-06-15 13:57:52 -0400

    update react and dependencies

--- src/enhancers/withParams.ts
+++ src/enhancers/withParams.ts
@@ -1,21 +1,20 @@
 import {EnhancerContext, EnhancerResult, generateAssignments} from '@truefit/bach';
 import {useParams} from 'react-router';
 
-export default <T>(params: Array<keyof T>) => ({
-  generateNewVariable,
-}: EnhancerContext): EnhancerResult => {
-  const paramsAlias = generateNewVariable();
-  const assignments = generateAssignments(params as string[], paramsAlias);
+export default <T>(params: Array<keyof T>) =>
+  ({generateNewVariable}: EnhancerContext): EnhancerResult => {
+    const paramsAlias = generateNewVariable();
+    const assignments = generateAssignments(params as string[], paramsAlias);
 
-  return {
-    dependencies: {
-      useParams,
-    },
-    initialize: `
+    return {
+      dependencies: {
+        useParams,
+      },
+      initialize: `
       const ${paramsAlias} = useParams();
 
       ${assignments}
     `,
-    props: params as string[],
+      props: params as string[],
+    };
   };
-};
